c93715ebee316077316f22e8048a4294ccf207f9
RealFictionStudio <realfictionstudio@gmail.com>
2023-12-10 18:20:19 +0100

    Version 1.2

--- mini_src/widgets/window.py
+++ mini_src/widgets/window.py
@@ -3,6 +3,7 @@ import customtkinter as ctk
 #from recorder import Recorder
 from editor import Editor
 from downloader import Downloader
+from player import Player
 
 #from load_config import load_config_settings
 
@@ -15,7 +16,7 @@ class Window:
         self.root = ctk.CTk()
         self.root.geometry("800x600")
         self.root.resizable(True, True)
-        self.root.title("FoxFM Studio v1.1")
+        self.root.title("FoxFM Studio v1.2")
         
         self.tabview = ctk.CTkTabview(master=self.root)
         self.tabview.pack(expand=True, fill="both")
@@ -24,6 +25,8 @@ class Window:
         #self.tabview.add("record")
         self.tabview.add("  edit  ")
         #self.tabview.add("upload")
+        self.tabview.add("player")
+
         self.tabview.set("download")
 
 
@@ -31,6 +34,7 @@ class Window:
         #Recorder(self.tabview.tab("record"))
         Editor(self.tabview.tab("  edit  "))
         Downloader(self.tabview.tab("download"), self.root)
+        Player(self.tabview.tab("player"))
         self.root.mainloop()
 
 
