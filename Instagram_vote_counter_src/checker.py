755975eb45083d6c8e5bd15eb92138b25bbca61e
RealFictionStudio <realfictionstudio@gmail.com>
2023-12-30 16:00:49 +0100

    New subproject preparation

---
+++ Instagram_vote_counter_src/checker.py
@@ -0,0 +1,7 @@
+def is_vote(text:str):
+    ...
+
+
+def count_votes(votes:dict[str:int], with_likes:bool):
+    vtexts = votes.keys()
+
---
+++ Instagram_vote_counter_src/window.py
@@ -0,0 +1,5 @@
+import customtkinter as ctk
+
+class VoteCounter:
+    def __init__(self) -> None:
+        pass
